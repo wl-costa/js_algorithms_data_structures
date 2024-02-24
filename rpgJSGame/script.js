acd89414c87e70e6ad972a45399e837f3b4e895c
Wendell Costa <wendellcosta.dev@gmail.com>
2024-02-24 17:10:19 -0300

    changes the #text inner text

--- rpgJSGame/script.js
+++ rpgJSGame/script.js
@@ -28,6 +28,7 @@ button3.onclick = fightDragon;
 // when the button is clicked, myFunction will be called.
 
 function goStore() {
+    text.innerText = "You enter the store.";
     button1.innerText = "Buy 10 health (10 gold)";
     button2.innerText = "Buy weapon (30 gold)";
     button3.innerText = "Go to town square";
