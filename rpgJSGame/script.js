bf6167798ebb614464c8795f871825bbea1575ce
Wendell Costa <wendellcosta.dev@gmail.com>
2024-02-24 19:35:33 -0300

    created arguments for update function and gave arguments to each location function

--- rpgJSGame/script.js
+++ rpgJSGame/script.js
@@ -35,29 +35,33 @@ const locations = [
 
 // functions are special tools that allow you to run sections of code at specific times.
 // initialize buttons
+// when the button is clicked, myFunction will be called.
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 //Functions can take parameters, which are values that are given to the function each time it runs.
 function update(location) {
-
+    button1.innerText = location["button text"][0];
+    button2.innerText = location["button text"][1];
+    button3.innerText = location["button text"][2];
+    button1.onclick = location["button functions"][0];
+    button2.onclick = location["button functions"][1];
+    button3.onclick = location["button functions"][2];
+    text.innerText = location.text;
 }
 
-// when the button is clicked, myFunction will be called.
+function goTown() {
+    update(locations[0]);
+}
+
 
 function goStore() {
-    text.innerText = "You enter the store.";
-    button1.innerText = "Buy 10 health (10 gold)";
-    button2.innerText = "Buy weapon (30 gold)";
-    button3.innerText = "Go to town square";
-    button1.onclick = buyHealth;
-    button2.onclick = buyWeapon;
-    button3.onclick = goTown;
+    update(locations[1]);
 }
 
 function goCave() {
-
+    update(locations[2]);
 }
 
 function fightDragon() {
@@ -70,14 +74,4 @@ function buyHealth() {
 
 function buyWeapon() {
 
-}
-
-function goTown() {
-    text.innerText = "You are in the town square. You see a sign that says \"Store\"."; // using \"\" wraps the text with double quotes
-    button1.innerText = "Go to store";
-    button2.innerText = "Go to cave";
-    button3.innerText = "Fight dragon";
-    button1.onclick = goStore;
-    button2.onclick = goCave;
-    button3.onclick = fightDragon;
 }
