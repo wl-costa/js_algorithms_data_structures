2360001989e672109b79adbf8a6a00cd76efbfdd
Wendell Costa <wendellcosta.dev@gmail.com>
2024-02-24 20:00:48 -0300

    created a new object named cave

--- rpgJSGame/script.js
+++ rpgJSGame/script.js
@@ -30,6 +30,12 @@ const locations = [
         "button functions": [buyHealth, buyWeapon, goTown],
         text: "You enter the store."
     },
+    {
+        name: "cave",
+        "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+        "button functions": [fightSlime, fightBeast, goTown],
+        text: "You enter the cave. You see some monsters."
+    }
 ];
 // here the use of const means that those value should never change
 
