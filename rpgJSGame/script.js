3122a5c39eac982d7d61b7cbd3248ba526c09cb1
Wendell Costa <wendellcosta.dev@gmail.com>
2024-02-24 18:44:23 -0300

    *

--- rpgJSGame/script.js
+++ rpgJSGame/script.js
@@ -21,7 +21,7 @@ const locations = [
     {
         name: "town square",
         "button text": ["Go to store", "Go to cave", "Fight dragon"],
-        "button functions": [ goStore, goCave, fightDragon],
+        "button functions": [goStore, goCave, fightDragon],
         text: "You are in the town square. You see a sign that says \"Store\"."
     }
 ];
