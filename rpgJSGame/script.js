074bc53f04a09ce819b9e7db19f66b99cc75bd01
Wendell Costa <wendellcosta.dev@gmail.com>
2024-02-25 16:10:14 -0300

    updated several functions

--- rpgJSGame/script.js
+++ rpgJSGame/script.js
@@ -149,13 +149,28 @@ function sellWeapon() {
 }
 
 function fightSlime() {
-
+    fighting = 0;
+    goFight();
 }
 
 function fightBeast() {
-
+    fighting = 1;
+    goFight();
 }
 
 function fightDragon() {
+    fighting = 2;
+    goFight();
+}
+
+function goFight() {
+
+}
+
+function attack() {
+
+}
+
+function dodge() {
 
 }
