def42ca64c0a881df12c84c60e00e1b9b3a5a4cc
Wendell Costa <wendellcosta.dev@gmail.com>
2024-02-26 15:09:27 -0300

    created a new locations array object as well as using update function inside defeatMonster function

--- rpgJSGame/script.js
+++ rpgJSGame/script.js
@@ -60,6 +60,12 @@ const locations = [
         "button text": ["Attack", "Dodge", "Run"],
         "button functions": [attack, dodge, goTown],
         text: "You are fighting a monster."
+    },
+    {
+        name: "kill monster",
+        "button text": ["Go to town square", "Go to town square", "Go to town square"],
+        "button functions": [goTown, goTown, goTown],
+        text: "The monster screams Arg! as it dies. You gain experience points and find gold."
     }
 ];
 const monsters = [
@@ -205,4 +211,5 @@ function defeatMonster() {
     xp += monsters[fighting].level;
     goldText.innerText = gold;
     xpText.innerText = xp;
+    update(locations[4]);
 }
