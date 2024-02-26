c4b59c62865ca6472a11abd6c1ca160aa195bbea
Wendell Costa <wendellcosta.dev@gmail.com>
2024-02-26 18:01:10 -0300

    solved a bug where the only player weapon could break

--- rpgJSGame/script.js
+++ rpgJSGame/script.js
@@ -216,7 +216,7 @@ function attack() {
             defeatMonster();
         }
     }
-    if (Math.random() <= .1) {
+    if (Math.random() <= .1 && inventory.length !== 1) {
         text.innerText += " Your " + inventory.pop() + " breaks." // used inventory.pop(), which will remove the last item in the array AND return it so it appears in the string.
         currentWeapon--;
     }
