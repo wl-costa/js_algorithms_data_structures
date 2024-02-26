c31b2186865b534bad342037d8d03bb2f0b52f2f
Wendell Costa <wendellcosta.dev@gmail.com>
2024-02-26 17:08:20 -0300

    added a new object into locations const array

--- rpgJSGame/script.js
+++ rpgJSGame/script.js
@@ -72,6 +72,12 @@ const locations = [
         "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
         "button functions": [restart, restart, restart],
         text: "You died. &#x2620 "
+    },
+    {
+        name: "win",
+        "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+        "button functions": [restart, restart, restart],
+        text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;"
     }
 ];
 const monsters = [
