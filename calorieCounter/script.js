351465d62e7c533c5be17f8b37637d69b95d6965
Wendell Costa <wendellcosta.dev@gmail.com>
2024-02-28 18:16:50 -0300

    declared a boolean variable set to false

--- calorieCounter/script.js
+++ calorieCounter/script.js
@@ -5,3 +5,4 @@ const entryDropdown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
+let isError = false;
