fc3c2c12fedee5a9c9a8f29864ecb9ad5afde1c6
Wendell Costa <wendellcosta.dev@gmail.com>
2024-02-28 18:12:38 -0300

    created a const to the budget and entry-dropdown id

--- calorieCounter/script.js
+++ calorieCounter/script.js
@@ -1,2 +1,4 @@
  // To access an HTML element with a given id name It can be used the getElementById() method.
 const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
